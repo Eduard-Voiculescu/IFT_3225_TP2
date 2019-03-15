0323ee2092b4149f9057b359e22bc924193f3e86
unknown <eduardvoiculescu95@gmail.com>
2019-03-14 20:14:58 -0400

    changed the initializing of the grid to null

--- js/Grid.js
+++ js/Grid.js
@@ -1,11 +1,10 @@
 class Grid {
     constructor(size) {
         this._gridSize = size || 4;
-        this._cells = new Array(this._gridSize);
-        this.initializeCells();
+        this._cells = [];
+        this.initializeCells(); // initialize the 2D game array
     }
 
-    /* ---------- Getters ---------- */
     get gridSize() {
         return this._gridSize;
     }
@@ -13,36 +12,36 @@ class Grid {
     get cells() {
         return this._cells;
     }
-    /* ---------- Getters ---------- */
 
+    // Inserts tile in cell[tile.posX][tile.posY]
     insertTile (tile) {
         this._cells[tile.posX][tile.posY] = tile;
     }
 
+    // check for the first available cell 
     availableCell(){
+        var position = [];
         for (var i = 0; i < this._gridSize; i++) {
             for (var j = 0; j < this._gridSize; j++) {
-                if (this._cells[i][j] != undefined) {
-                    return this._cells[i][j]; 
+                if (this._cells[i][j] == null) {
+                    position[0] = i;
+                    position[1] = j;
+                    return position; 
                 } 
             }
         }
         return null; // no available cells
     }
 
+    // initialize the n x n grid with null objects 
     initializeCells() {
         for (var i = 0; i < this._gridSize; i++) {
-            this._cells[i] = new Array(this._gridSize); // create an n x n matrix
+            this._cells[i] = []; // create an n x n matrix
+            for (var j = 0; j < this._gridSize; j++) {
+                this._cells[i][j] = null;
+            }
         }
     }
-    
-    randomValueGenerator (){
-        if (Math.random() < 0.50) {
-            return 2;
-        } else {
-            return 4;
-        }
-    };
 
     randomPositionInitializer() {
         return Math.floor(Math.random() * this._gridSize);
@@ -53,7 +52,11 @@ class Grid {
         for (var i = 0; i < this._gridSize; i++){
             string += "+"
             for(var j = 0; j < this._gridSize; j++){
-                string += "  " + this._cells[i][j].toString() + "  +";
+                if (this._cells[i][j] == null) {
+                    string += "     +";
+                } else {
+                    string += "  " + this._cells[i][j].toString() + "  +";
+                }
             }
             string += "\n";
         }
