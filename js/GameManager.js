7c3f872c36be8ba79689ccaf10b4815f760fad71
unknown <eduardvoiculescu95@gmail.com>
2019-03-17 00:15:46 -0400

    adding the keyboard manager with all the functions associated to it

--- js/GameManager.js
+++ js/GameManager.js
@@ -3,40 +3,183 @@ class GameManager {
         this._size = size || 4;
         this.startTiles = 2; // number of starting tiles 
         this._grid; // game grid
-        this._keyboardManager = new KeyboardManager();
-        this.initializeGrid();
-        this.setUpGame();
+        this._colors = [];
+        this._hasMoved = false; // bug si la personne bougeait mais rien se passait
         initializeHTMLGrid();
+        this.keyboardPlayer();
+        this.initializeGrid();
+        this.setupGame();
+    }
+
+    /* Keyboard listener */
+    keyboardPlayer() {
+        window.addEventListener("keydown", key => {
+            if (key.keyCode == 37 || key.keyCode == 65) {
+                this.left()
+            } else if (key.keyCode == 38 || key.keyCode == 87) {
+                this.up();
+            } else if (key.keyCode == 39 || key.keyCode == 68) {
+                this.right();
+            } else if (key.keyCode == 40 || key.keyCode == 83) {
+                this.down();
+            }
+            if (this._hasMoved) {
+                var randomTile = this.addRandomTile();
+                while (this._grid.cells[randomTile.posX][randomTile.posY].tileValue != 0) {
+                    randomTile = this.addRandomTile();
+                }
+                this._grid.insertTile(randomTile);
+                this._hasMoved = false;
+            }
+            this.toString();
+            console.log(this._grid);
+            for (var i = 0; i < this._size; i++) {
+                for (var j = 0; j < this._size; j++) {
+                    console.log(this._grid.cells[i][j]);  
+                    this._grid.updateGrid(this._grid.cells[i][j]);
+                }
+            }
+            // this.toString();
+            // console.log(this._grid);
+        });
+    }
+    left () {
+        for (var i = 0; i < this._size; i++) {
+            for (var j = 1; j < this._size; j++){ // the first column cannot go the the left
+                console.log("left -> i: " + i + " j : " + j);
+                if (this._grid.cells[i][j].tileValue != 0){
+                    var column = j;
+                    var value = this._grid.cells[i][column].tileValue;
+
+                    // slide the Tile to the left until we get to the end or if we have to merge with another Tile
+                    while (column > 0 && this._grid.cells[i][column - 1].tileValue == 0) {
+                        this._grid.cells[i][column - 1].tileValue = value;
+                        this._grid.cells[i][column].tileValue = 0;
+                        column--;
+                        this._hasMoved = true; 
+                    }
+
+                    // we have reached a Tile that has to be merged
+                    if (column >= 1 && value == this._grid.cells[i][column - 1]) {
+                        this._grid.cells[i][column - 1].tileValue = value * 2;
+                        this._grid.cells[i][column].tileValue = 0;
+                        this._hasMoved = true;
+                    }
+                }
+            }
+        }
+        console.log("left button pressed");
+    }
+
+    up() {
+        for (var i = 1; i < this._size; i++) {
+            for (var j = 0; j < this._size; j++) {
+                console.log("up -> i: " + i + " j : " + j);
+                if (this._grid.cells[i][j].tileValue != 0) {
+                    var row = i;
+                    var value = this._grid.cells[row][j].tileValue;
+
+                    // slide the Tile up untile we get to the end or if we have to merge with another Tile
+                    while (row > 0 && this._grid.cells[row - 1][j].tileValue == 0){
+                        this._grid.cells[row - 1][j].tileValue = value;
+                        this._grid.cells[row][j].tileValue = 0;                        
+                        row--;
+                        this._hasMoved = true; 
+                    }
+
+                    // we have reached a TIle that has to be merged
+                    if (row >= 1 && value == this._grid.cells[row - 1][j]) {
+                        this._grid.cells[row - 1][j].tileValue = value * 2;
+                        this._grid.cells[row][j].tileValue = 0;
+                        this._hasMoved = true; 
+                    }
+                }
+            }
+        }
+        console.log("up button pressed");
+    }
+
+    down() {
+        for (var i = this._size - 2; i >= 0; i--) {
+            for (var j = 0; j < this._size; j++) {
+                console.log("down -> i: " + i + " j : " + j);
+                if (this._grid.cells[i][j].tileValue != 0) {
+                    var row = i;
+                    var value = this._grid.cells[row][j].tileValue;
+                    
+                    // slide the Tile down until we get to the end or if we have to merge with another Tile
+                    while (row < this._size - 1 && this._grid.cells[row + 1][j] == 0) {
+                        this._grid.cells[row + 1][j].tileValue = value;
+                        this._grid.cells[row][j].tileValue = 0;
+                        row++;
+                        this._hasMoved = true; 
+                    }
+    
+                    // we have reacehd a Tile that has to be merged
+                    if (row < this._size - 1 && value == this._grid.cells[row][j]) {
+                        this._grid.cells[row + 1][j].tileValue = value * 2;
+                        this._grid.cells[row][j].tileValue = 0;
+                        this._hasMoved = true; 
+                    }
+                }
+            }
+        }
+        console.log("down button pressed");
+    }
+
+    right() {
+        for (var i = 0; i < this._size; i++) {
+            for (var j = this._size - 2; j >= 0; j--) { // the last column cannot go to the right
+                console.log("right -> i: " + i + " j : " + j);
+                if (this._grid.cells[i][j].tileValue != 0) {
+                    var column = j; // la colonne actuelle où la Tile se trouve
+                    var value = this._grid.cells[i][column].tileValue;
+                    
+                    // slide the Tile to the right until we get to the end or if we have to merge with another Tile
+                    while (column < this._size - 1 && this._grid.cells[i][column + 1].tileValue == 0) {
+                        this._grid.cells[i][column + 1].tileValue = value;
+                        this._grid.cells[i][column].tileValue = 0; 
+                        column++;
+                        this._hasMoved = true; 
+                    }
+
+                    // we have reached a Tile that has to be merged
+                    if (column <= this._size -2 && value == this._grid.cells[i][column + 1]) {
+                        this._grid.cells[i][column + 1].tileValue = value * 2; 
+                        this._grid.cells[i][column].tileValue = 0;  
+                        this._hasMoved = true;   
+                    }
+                }
+            }
+        }
+        console.log("right button pressed");
     }
 
     initializeGrid() {
         this._grid = new Grid(this._size);
         console.log(this._grid);
-        
         console.log("Grid size is : " + this._size);
     }
 
     // Add the starting tiles
-    setUpStartingTiles() {
-        var startingTiles = this.addRandomTile();
-        console.log(startingTiles);
-        for (var i = 0; i < startingTiles.length; i++) {
-            this._grid.insertTile(startingTiles[i]);
+    setupStartingTiles() {
+        // var startingTiles = this.addRandomTile();
+        var startingTile1 = this.addRandomTile();
+        var startingTile2 = this.addRandomTile();
+        while (startingTile1.posX == startingTile2.posX || startingTile1.posY == startingTile2.posY){
+            startingTile2 = this.addRandomTile();
         }
+        this._grid.insertTile(startingTile1);
+        this._grid.insertTile(startingTile2);
     }
 
     // add 2 random tiles to the grid
     addRandomTile() {
-        var tile1 = this.chooseRandomTile();
-        var tile2 = this.chooseRandomTile();
-        while (tile1[0] == tile2[0] || tile1[1] == tile2[1]) {
-            tile2 = this.chooseRandomTile();
-        }
-        return [new Tile(tile1[0], tile1[1], this.randomValueGenerator()), 
-                new Tile(tile2[0], tile2[1], this.randomValueGenerator())]
+        var position = this.chooseRandomTilePostion();
+        return new Tile(position[0], position[1], this.randomValueGenerator());
     }
 
-    chooseRandomTile() {
+    chooseRandomTilePostion() {
         var position = [];
         position[0] = Math.floor(Math.random() * this._size)
         position[1] = Math.floor(Math.random() * this._size)
@@ -56,8 +199,8 @@ class GameManager {
     }
 
     // Start 2048 game
-    setUpGame(){
-        this.setUpStartingTiles();
+    setupGame(){
+        this.setupStartingTiles();
         this.startGame();
     }
 
