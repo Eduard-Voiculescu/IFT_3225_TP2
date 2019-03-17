1896f530114b59a216164dfdbc9d1bd32de4bc97
unknown <eduardvoiculescu95@gmail.com>
2019-03-17 11:35:08 -0400

    updated the updateGrid function to match colors of cells

--- js/Grid.js
+++ js/Grid.js
@@ -2,6 +2,10 @@ class Grid {
     constructor(size) {
         this._gridSize = size || 4;
         this._cells = [];
+        // [2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048]
+        this._colors = ["#eee4da", "#ede0c8", "#f2b179","#f59563", "#f67c5f", 
+                        "#f65e3b", "#edcf72", "#edcc61", "#edc850", "#edc53f", "#edc22e"];
+
         this.initializeCells(); // initialize the 2D game array
     }
 
@@ -50,11 +54,18 @@ class Grid {
 
     /* @tile : Tile */
     updateGrid (tile) {
+        /** changer le nombre de moves et le nombre de score */
         if (tile.tileValue != 0) {
             document.getElementById("grid-item-" + tile.posX + "-" + tile.posY).innerHTML = tile.tileValue;
+            document.getElementById("grid-item-" + tile.posX + "-" + tile.posY).style.background = this.chooseColor(tile.tileValue);
         } else {
             document.getElementById("grid-item-" + tile.posX + "-" + tile.posY).innerHTML = "";
+            document.getElementById("grid-item-" + tile.posX + "-" + tile.posY).style.background = "#eee4da59";
         }
+    }
+
+    chooseColor (tileValue) {
+        return this._colors[(Math.log2(tileValue) - 1) % this._colors.length];
     }
 
     toString(){
